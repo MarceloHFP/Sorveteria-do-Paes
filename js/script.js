c900228d419038fddae2b17fc370ca177653026e
marcelohfp <marcelo.paes142@gmail.com>
2024-02-23 17:12:48 -0300

    fix errors

--- js/script.js
+++
@@ -1,6 +0,0 @@
-sorveteJson.map((item, index ) => {
-    console.log(item)
-    let sorvItem = document.querySelector('.models .card-item').cloneNode(true)
-
-    document.querySelector('.itemArea').append(sorvItem)
-})
